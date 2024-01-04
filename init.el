4b5c7c45dd8f5573f812754314353ce64149509e
Schmitz, Arne <arne.schmitz@cerence.com>
2024-01-04 13:34:55 +0100

    switch to eglot. FIXME: hard coded clangd path

--- init.el
+++ init.el
@@ -190,60 +190,16 @@
   ;; ReStructured Text
   ;;
   (add-to-list 'auto-mode-alist '("\\.hts\\'" . rst-mode))
-  
+
   ;;
-  ;; rtags
+  ;; eglot
   ;;
-  (use-package rtags
+  (use-package eglot
     :ensure t)
-  (add-hook 'c-mode-hook 'rtags-start-process-unless-running)
-  (add-hook 'c++-mode-hook 'rtags-start-process-unless-running)
-  (add-hook 'objc-mode-hook 'rtags-start-process-unless-running)
-  (rtags-enable-standard-keybindings)
-  (defun use-rtags (&optional useFileManager)
-    (and (rtags-executable-find "rc")
-         (cond ((not (gtags-get-rootpath)) t)
-               ((and (not (eq major-mode 'c++-mode))
-                     (not (eq major-mode 'c-mode))) (rtags-has-filemanager))
-               (useFileManager (rtags-has-filemanager))
-               (t (rtags-is-indexed)))))
-
-  (defun tags-find-symbol-at-point (&optional prefix)
-    (interactive "P")
-    (if (and (not (rtags-find-symbol-at-point prefix)) rtags-last-request-not-indexed)
-        (gtags-find-tag)))
-  (defun tags-find-references-at-point (&optional prefix)
-    (interactive "P")
-    (if (and (not (rtags-find-references-at-point prefix)) rtags-last-request-not-indexed)
-        (gtags-find-rtag)))
-  (defun tags-find-symbol ()
-    (interactive)
-    (call-interactively (if (use-rtags) 'rtags-find-symbol 'gtags-find-symbol)))
-  (defun tags-find-references ()
-    (interactive)
-    (call-interactively (if (use-rtags) 'rtags-find-references 'gtags-find-rtag)))
-  (defun tags-find-file ()
-    (interactive)
-    (call-interactively (if (use-rtags t) 'rtags-find-file 'gtags-find-file)))
-  (defun tags-imenu ()
-    (interactive)
-    (call-interactively (if (use-rtags t) 'rtags-imenu 'idomenu)))
-
-  (define-key c-mode-base-map (kbd "M-.") (function rtags-find-symbol-at-point))
-  (define-key c-mode-base-map (kbd "M-,") (function rtags-find-references-at-point))
-  (define-key c-mode-base-map (kbd "M-;") (function rtags-find-file))
-  (define-key c-mode-base-map (kbd "C-.") (function rtags-find-symbol))
-  (define-key c-mode-base-map (kbd "C-,") (function rtags-find-references))
-  (define-key c-mode-base-map (kbd "C-<") (function rtags-find-virtuals-at-point))
-  (define-key c-mode-base-map (kbd "M-i") (function tags-imenu))
-
-  (define-key global-map (kbd "M-.") (function tags-find-symbol-at-point))
-  (define-key global-map (kbd "M-,") (function tags-find-references-at-point))
-  (define-key global-map (kbd "M-;") (function tags-find-file))
-  (define-key global-map (kbd "C-.") (function tags-find-symbol))
-  (define-key global-map (kbd "C-,") (function tags-find-references))
-  (define-key global-map (kbd "C-<") (function rtags-find-virtuals-at-point))
-  (define-key global-map (kbd "M-i") (function tags-imenu))
+  (add-to-list 'eglot-server-programs '((c++-mode c-mode) "/home/linuxbrew/.linuxbrew/opt/llvm@13/bin/clangd"))
+  (add-hook 'c-mode-hook 'eglot-ensure)
+  (add-hook 'c++-mode-hook 'eglot-ensure)
+ 
 
   ;; Syntax checking.
   ;; Should be automatic.
@@ -252,18 +208,7 @@
     :ensure t
     :config
     (global-flycheck-mode))
-
-  (use-package flycheck-clangcheck
-    :ensure t)
-  (defun my-select-clangcheck-for-checker ()
-    "Select clang-check for flycheck's checker."
-    (flycheck-set-checker-executable 'c/c++-clangcheck
-                                     "/home/linuxbrew/.linuxbrew/opt/llvm@13/bin/clang-check")
-    (flycheck-select-checker 'c/c++-clangcheck))
-
-  (add-hook 'c-mode-hook #'my-select-clangcheck-for-checker)
-  (add-hook 'c++-mode-hook #'my-select-clangcheck-for-checker)
-  
+    
   ;; (use-package flycheck-rtags
   ;;   :ensure t)
   ;; (setq rtags-autostart-diagnostics t)
